c53edc3f72c7c552399cb938804ca6c17f06b040
Srilatha <koduri0311@gmail.com>
2024-11-04 17:36:50 +0000

    kubernetes

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0e529c7aeda1ef90f"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0f745b1f2846b188f"
+}
